b76c2d36ceec078002cab9c7216d26b4862e140e
Ostap <Ostapkriven13@gmail.com>
2023-06-20 18:52:23 +0300

    і
    і

--- css/js/01-gallery.js
+++ css/js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = item.original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.preview;
+  image.setAttribute('data-source', item.original);
+  image.alt = item.description;
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+}
+
+function openModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageSrc = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<img src="${largeImageSrc}" alt="Full Size Image">`
+  );
+
+  instance.show();
+}
+
+gallery.addEventListener('click', openModal);
+
+galleryItems.forEach(item => {
+  const galleryItem = createGalleryItem(item);
+  gallery.appendChild(galleryItem);
+});
