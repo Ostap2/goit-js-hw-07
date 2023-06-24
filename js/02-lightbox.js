d7c525c662d2c720f0f942a0c52b6173d255a356
Ostap <Ostapkriven13@gmail.com>
2023-06-24 09:39:49 +0300

    і
    і

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -14,7 +14,6 @@ const createGalleryItem = ({ preview, original, description }) => {
   galleryImage.classList.add('gallery__image');
   galleryImage.src = preview;
   galleryImage.alt = description;
-  galleryImage.dataset.source = original;
 
   galleryLink.appendChild(galleryImage);
   galleryItem.appendChild(galleryLink);
@@ -25,30 +24,17 @@ const createGalleryItem = ({ preview, original, description }) => {
 const galleryItemsMarkup = galleryItems.map(createGalleryItem);
 gallery.append(...galleryItemsMarkup);
 
-gallery.addEventListener('click', handleGalleryClick);
-
-function handleGalleryClick(event) {
-  event.preventDefault();
-
-  const { target } = event;
-  if (target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const largeImageUrl = target.dataset.source;
-
-  const instance = basicLightbox.create(`
-    <img src="${largeImageUrl}" width="800" height="600">
-  `);
-
-  instance.show();
-
-  window.addEventListener('keydown', handleKeyPress);
-
-  function handleKeyPress(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-      window.removeEventListener('keydown', handleKeyPress);
-    }
-  }
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionsDelayMultiplier: 1.5,
+    elementsSelector: '.gallery__link', // Додали селектор елементів галереї
+    docClose: false, // Вимкнули закриття модального вікна при кліку на документ
+    nav: true, // Включили перемикачі вліво/вправо
+    navText: ['&larr;', '&rarr;'], // Текст для кнопок перемикачів
+    navClass: ['simple-lightbox__nav--prev', 'simple-lightbox__nav--next'], // Класи для кнопок перемикачів
+  });
+});
