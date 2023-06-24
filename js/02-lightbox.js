6f8547bd501c55e3e82fba77f0e71473f9edcebd
Ostap <Ostapkriven13@gmail.com>
2023-06-24 09:25:51 +0300

    і
    і

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -53,17 +53,3 @@ function handleGalleryClick(event) {
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionDelay: 250,
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionsDelayMultiplier: 1.5,
-    elementsSelector: '.gallery__link', // Додали селектор елементів галереї
-    docClose: false, // Вимкнули закриття модального вікна при кліку на документ
-    nav: true, // Включили перемикачі вліво/вправо
-    navText: ['&larr;', '&rarr;'], // Текст для кнопок перемикачів
-    navClass: ['simple-lightbox__nav--prev', 'simple-lightbox__nav--next'], // Класи для кнопок перемикачів
-  });
-});
