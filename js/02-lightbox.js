98bed39192e86904913b47259fc54d574c3bd397
Ostap <Ostapkriven13@gmail.com>
2023-06-24 09:51:30 +0300

    і
    і

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 
+const galleryItems = [
+  // Gallery items data here
+];
+
 const gallery = document.querySelector('.gallery');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
 
 const createGalleryItem = ({ preview, original, description }) => {
   const galleryItem = document.createElement('li');
@@ -24,64 +30,15 @@ const createGalleryItem = ({ preview, original, description }) => {
 const galleryItemsMarkup = galleryItems.map(createGalleryItem);
 gallery.append(...galleryItemsMarkup);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionDelay: 250,
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionsDelayMultiplier: 1.5,
-    elementsSelector: '.gallery__link', // Додали селектор елементів галереї
-    docClose: false, // Вимкнули закриття модального вікна при кліку на документ
-    nav: true, // Включили перемикачі вліво/вправо
-    navText: ['&larr;', '&rarr;'], // Текст для кнопок перемикачів
-    navClass: ['simple-lightbox__nav--prev', 'simple-lightbox__nav--next'], // Класи для кнопок перемикачів
-  });
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250
 });
 
+prevButton.addEventListener('click', () => {
+  lightbox.prev();
+});
 
-
-
-
- const galleryItems = [
-      // Gallery items data here
-    ];
-
-    const gallery = document.querySelector('.gallery');
-    const prevButton = document.getElementById('prevButton');
-    const nextButton = document.getElementById('nextButton');
-
-    const createGalleryItem = ({ preview, original, description }) => {
-      const galleryItem = document.createElement('li');
-      galleryItem.classList.add('gallery__item');
-
-      const galleryLink = document.createElement('a');
-      galleryLink.classList.add('gallery__link');
-      galleryLink.href = original;
-
-      const galleryImage = document.createElement('img');
-      galleryImage.classList.add('gallery__image');
-      galleryImage.src = preview;
-      galleryImage.alt = description;
-
-      galleryLink.appendChild(galleryImage);
-      galleryItem.appendChild(galleryLink);
-
-      return galleryItem;
-    };
-
-    const galleryItemsMarkup = galleryItems.map(createGalleryItem);
-    gallery.append(...galleryItemsMarkup);
-
-    const lightbox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250
-    });
-
-    prevButton.addEventListener('click', () => {
-      lightbox.prev();
-    });
-
-    nextButton.addEventListener('click', () => {
-      lightbox.next();
-    });
+nextButton.addEventListener('click', () => {
+  lightbox.next();
+});
