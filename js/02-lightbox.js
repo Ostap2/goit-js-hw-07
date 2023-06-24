b84851099f8606c9d089c56bede46531e16a48be
Ostap <Ostapkriven13@gmail.com>
2023-06-24 10:54:20 +0300

    s
    s

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -54,23 +54,19 @@ function handleGalleryClick(event) {
 }
 
 
-// Створити кнопку "Наступна"
-var nextButton = document.createElement('button');
-nextButton.textContent = 'Наступна';
+// Отримати посилання на елементи, на які ви бажаєте додати переключення
+var nextButton = document.querySelector('.next-button');
+var prevButton = document.querySelector('.prev-button');
+
+// Підключитись до екземпляра SimpleLightbox
+var lightbox = new SimpleLightbox('.gallery');
+
+// Додати обробник подій для наступної фотографії
 nextButton.addEventListener('click', function() {
   lightbox.next();
 });
 
-// Створити кнопку "Попередня"
-var prevButton = document.createElement('button');
-prevButton.textContent = 'Попередня';
+// Додати обробник подій для попередньої фотографії
 prevButton.addEventListener('click', function() {
   lightbox.prev();
 });
-
-// Знайти елемент, до якого потрібно додати кнопки
-var galleryContainer = document.querySelector('.gallery-container');
-
-// Додати кнопки до контейнера
-galleryContainer.appendChild(prevButton);
-galleryContainer.appendChild(nextButton);
