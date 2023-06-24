dc1801c725d83b5d1c12c9e55adc819f1827b6dd
Ostap <Ostapkriven13@gmail.com>
2023-06-24 13:02:36 +0300

    і
    і

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,58 +8,57 @@ const lightbox = new SimpleLightbox('.gallery a', {
   scrollZoom: false,
   });
 
-
-
-
-const gallery = document.querySelector('.gallery');
-
-const createGalleryItem = ({ preview, original, description }) => {
-  const galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
-
-  const galleryLink = document.createElement('a');
-  galleryLink.classList.add('gallery__link');
-  galleryLink.href = original;
-
-  const galleryImage = document.createElement('img');
-  galleryImage.classList.add('gallery__image');
-  galleryImage.src = preview;
-  galleryImage.alt = description;
-  galleryImage.dataset.source = original;
-
-  galleryLink.appendChild(galleryImage);
-  galleryItem.appendChild(galleryLink);
-
-  return galleryItem;
-};
-
-const galleryItemsMarkup = galleryItems.map(createGalleryItem);
-gallery.append(...galleryItemsMarkup);
-
-gallery.addEventListener('click', handleGalleryClick);
-
-function handleGalleryClick(event) {
-  event.preventDefault();
-
-  const { target } = event;
-  if (target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const largeImageUrl = target.dataset.source;
-
-  const instance = basicLightbox.create(`
-    <img src="${largeImageUrl}" width="800" height="600">
-  `);
-
-  instance.show();
-
-  window.addEventListener('keydown', handleKeyPress);
-
-  function handleKeyPress(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-      window.removeEventListener('keydown', handleKeyPress);
+  document.addEventListener("DOMContentLoaded", function () {
+    // Масив даних для галереї
+    var galleryItems = [
+      {
+        small: "small-image-1.jpg",
+        large: "large-image-1.jpg",
+        alt: "Image description 1"
+      },
+      {
+        small: "small-image-2.jpg",
+        large: "large-image-2.jpg",
+        alt: "Image description 2"
+      },
+      // Додайте решту елементів галереї
+    ];
+  
+    // Функція для створення елемента галереї
+    function createGalleryItem(item) {
+      var listItem = document.createElement("li");
+      listItem.classList.add("gallery__item");
+  
+      var link = document.createElement("a");
+      link.classList.add("gallery__link");
+      link.href = item.large;
+  
+      var image = document.createElement("img");
+      image.classList.add("gallery__image");
+      image.src = item.small;
+      image.alt = item.alt;
+  
+      link.appendChild(image);
+      listItem.appendChild(link);
+  
+      return listItem;
     }
-  }
-}
+  
+    // Отримання посилання на ul.gallery
+    var gallery = document.querySelector(".gallery");
+  
+    // Додавання елементів галереї
+    galleryItems.forEach(function (item) {
+      var galleryItem = createGalleryItem(item);
+      gallery.appendChild(galleryItem);
+    });
+  
+    // Підключення бібліотеки SimpleLightbox
+    var lightbox = new SimpleLightbox(".gallery a", {
+      captions: true,               // Включити підписи
+      captionsData: "alt",          // Використовувати атрибут alt для підписів
+      captionPosition: "bottom",    // Позиція підпису: 'top', 'bottom', 'left', 'right'
+      captionDelay: 250             // Затримка перед відображенням підпису (у мілісекундах)
+    });
+  });
+  
