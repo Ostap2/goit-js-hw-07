9cae4c4e01af279560128d3188d64b9e6ebe2dad
Ostap <Ostapkriven13@gmail.com>
2023-06-24 12:12:46 +0300

    і
    і

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,57 +1,63 @@
 import { galleryItems } from './gallery-items.js';
-// Масив даних галереї
-var galleryItems = [
-  {
-    preview: 'small-image-1.jpg',
-    original: 'large-image-1.jpg',
-    description: 'Image 1'
-  },
-  {
-    preview: 'small-image-2.jpg',
-    original: 'large-image-2.jpg',
-    description: 'Image 2'
-  },
-  {
-    preview: 'small-image-3.jpg',
-    original: 'large-image-3.jpg',
-    description: 'Image 3'
-  }
-];
 
-// Отримати посилання на елемент галереї
-var gallery = document.querySelector('.gallery');
 
-// Створити розмітку галереї на підставі даних
-function createGalleryMarkup(items) {
-  return items.map(function(item) {
-    return `
-      <li class="gallery__item">
-        <a class="gallery__link" href="${item.original}">
-          <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-        </a>
-      </li>
-    `;
-  }).join('');
-}
 
-// Додати розмітку галереї до елементу галереї
-function renderGallery(items) {
-  var galleryMarkup = createGalleryMarkup(items);
-  gallery.innerHTML = galleryMarkup;
-}
-
-// Ініціалізувати бібліотеку SimpleLightbox
-function initializeLightbox() {
-  var lightbox = new SimpleLightbox('.gallery__link', {
-    captions: true,
-    captionDelay: 250,
-    captionPosition: 'bottom',
-    captionsData: 'alt'
-  });
-}
-
-// Рендер галереї та ініціалізація бібліотеки після завантаження сторінки
-window.addEventListener('DOMContentLoaded', function() {
-  renderGallery(galleryItems);
-  initializeLightbox();
+document.addEventListener('DOMContentLoaded', function() {
+  const lightbox = new SimpleLightbox('.gallery a');
 });
+
+
+
+
+const gallery = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = preview;
+  galleryImage.alt = description;
+  galleryImage.dataset.source = original;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+};
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItem);
+gallery.append(...galleryItemsMarkup);
+
+gallery.addEventListener('click', handleGalleryClick);
+
+function handleGalleryClick(event) {
+  event.preventDefault();
+
+  const { target } = event;
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageUrl = target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  window.addEventListener('keydown', handleKeyPress);
+
+  function handleKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', handleKeyPress);
+    }
+  }
+}
