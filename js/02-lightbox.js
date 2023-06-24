1a789bbf0ed2fe1e8afbc1ec5c22402ac56f2cc4
Ostap <Ostapkriven13@gmail.com>
2023-06-24 09:55:17 +0300

    і
    і

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -42,3 +42,77 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+   
+
+const galleryItems = [
+  // Gallery items data here
+];
+
+const gallery = document.querySelector('.
+
+const galleryItems = [
+  // Gallery items data here
+];
+
+const gallery = document.querySelector('.gallery');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton
+
+const galleryItems = [
+  // Gallery items data here
+];
+
+const gallery = document.querySelector('.gallery');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  const galleryItem = document.createElement('li
+
+const galleryItems = [
+  // Gallery items data here
+];
+
+const gallery = document.querySelector('.gallery');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = preview;
+  galleryImage.alt = description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+};
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItem);
+gallery.append(...galleryItemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250
+});
+
+const prevImage = () => {
+  lightbox.close();
+  lightbox.prev();
+};
+
+const nextImage = () => {
+  lightbox.close();
+  lightbox.next();
+};
+
+prevButton.addEventListener('click', prevImage);
+nextButton.addEventListener('click', nextImage);
